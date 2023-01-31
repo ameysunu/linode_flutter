8247a2d716a13cef0729598b81b8520e2e4b487c
ameysunu <ameysunu00719@gmail.com>
2023-01-31 22:08:52 +0000

    build: spotify garbage work in progress

--- lib/controllers/spotify.dart
+++ lib/controllers/spotify.dart
@@ -3,10 +3,37 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:linode_flutter/controllers/secrets.dart';
 
-final String url = 'https://api.spotify.com/v1/tracks';
+var imageUrl;
+
+// final String url = 'https://api.spotify.com/v1/tracks';
+// final String accept = 'application/json';
+// final String contentType = 'application/json';
+// final String token = SPOTIFY_TOKEN;
+// final Map<String, String> headers = {
+//   'Accept': accept,
+//   'Content-Type': contentType,
+//   'Authorization': 'Bearer $token',
+// };
+
+// final Map<String, String> parameters = {
+//   'market': 'US',
+//   //'ids': '$SPOTIFY_ID_1, $SPOTIFY_ID_2, $SPOTIFY_ID_3',
+//   'ids': '7ouMYWpwJ422jRcDASZB7P,4VqPOruhp5EdPBeR92t6lQ,2takcwOaAZWiXQijPHIx7B'
+// };
+
+// Future<http.Response> getTracks() async {
+//   final String host = 'api.spotify.com';
+//   final String path = '/v1/tracks';
+//   final Uri uri =
+//       Uri(scheme: 'https', host: host, path: path, queryParameters: parameters);
+//   final response = await http.get(uri, headers: headers);
+//   return response;
+// }
+final String url = 'https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl';
 final String accept = 'application/json';
 final String contentType = 'application/json';
-final String token = SPOTIFY_TOKEN;
+final String token =
+    'BQD8C47ROgbbbSyvHKjRgbvF1a80iqV4UDDPcE-STWys1FenU-B28fkIn0RqUFdGFwx_ULKK1RcRAsoF9CsOE7IiJ_rwIexWs1rV0KstE_oNog1FqxKeppnvr3uSN89_lFKdenrS5gTe2bOrs_3kR5GOaN1PhX3_scGLbmXVX09kx_aIRb0MZSg7WMgQ2W-H';
 final Map<String, String> headers = {
   'Accept': accept,
   'Content-Type': contentType,
@@ -14,13 +41,12 @@ final Map<String, String> headers = {
 };
 
 final Map<String, String> parameters = {
-  'market': 'US',
-  'ids': '$SPOTIFY_ID_1, $SPOTIFY_ID_2, $SPOTIFY_ID_3',
+  'market': 'ES',
 };
 
 Future<http.Response> getTracks() async {
   final String host = 'api.spotify.com';
-  final String path = '/v1/tracks';
+  final String path = '/v1/tracks/11dFghVXANMlKmJXsNCbNl';
   final Uri uri =
       Uri(scheme: 'https', host: host, path: path, queryParameters: parameters);
   final response = await http.get(uri, headers: headers);
@@ -31,8 +57,35 @@ getValue() async {
   final response = await getTracks();
   if (response.statusCode == 200) {
     final jsonData = jsonDecode(response.body);
+    //print(jsonData["tracks"][0]["artists"][0]["uri"]);
     print(jsonData);
+    // print(jsonData["tracks"][0]["album"]);
+    // imageUrl = (jsonData["tracks"][0]["album"]["images"][0]["url"]);
+    // print(imageUrl);
+
+    print(jsonData);
+  } else {
+    print("Error : ${response.statusCode}");
+  }
+}
+
+getImageURL() async {
+  final response = await getTracks();
+  if (response.statusCode == 200) {
+    final jsonData = jsonDecode(response.body);
+    return jsonData["tracks"][0]["album"]["images"][0]["url"];
   } else {
     print("Error");
   }
 }
+
+// getScanCode() async {
+//   final response = await getTracks();
+//   if (response.statusCode == 200) {
+//     final jsonData = jsonDecode(response.body);
+//     final jsonURI = jsonData["tracks"][0]["artists"][0]["uri"];
+//     return 'https://scannables.scdn.co/uri/plain/jpeg/ffffff/black/640/${jsonURI}';
+//   } else {
+//     print("Error");
+//   }
+// }
--- lib/home.dart
+++ lib/home.dart
@@ -15,6 +15,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  void initState() {
+    getImageURL();
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) => getValue());
+  }
+
   @override
   Widget build(BuildContext context) {
     const backgroundColor = const Color(0xFFB392E4F);
@@ -35,78 +41,161 @@ class _HomePageState extends State<HomePage> {
           automaticallyImplyLeading: false,
         ),
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Welcome!",
-                  style: GoogleFonts.manrope(
-                      textStyle: TextStyle(color: Colors.white, fontSize: 30)),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 15.0),
-                  child: Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.all(20.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15.0),
-                      color: Colors.white,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Analyze my mood',
-                              style: GoogleFonts.manrope(
-                                  textStyle: TextStyle(fontSize: 20)),
-                            ),
-                            const Text("See if I'm groovy today!")
-                          ],
-                        ),
-                        Icon(Icons.music_note)
-                      ],
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Welcome!",
+                    style: GoogleFonts.manrope(
+                        textStyle:
+                            TextStyle(color: Colors.white, fontSize: 30)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15.0),
+                    child: Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.0),
+                        color: Colors.white,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Analyze my mood',
+                                style: GoogleFonts.manrope(
+                                    textStyle: TextStyle(fontSize: 20)),
+                              ),
+                              const Text("See if I'm groovy today!")
+                            ],
+                          ),
+                          Icon(Icons.music_note)
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 15.0),
-                  child: Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.all(20.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15.0),
-                      color: Colors.white,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Things I did',
-                              style: GoogleFonts.manrope(
-                                  textStyle: TextStyle(fontSize: 20)),
-                            ),
-                            const Text("All my previous mood swings!")
-                          ],
-                        ),
-                        Icon(Icons.security)
-                      ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15.0),
+                    child: Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.0),
+                        color: Colors.white,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Things I did',
+                                style: GoogleFonts.manrope(
+                                    textStyle: TextStyle(fontSize: 20)),
+                              ),
+                              const Text("All my previous mood swings!")
+                            ],
+                          ),
+                          Icon(Icons.security)
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                ElevatedButton(
-                    onPressed: () async {
-                      getValue();
-                    },
-                    child: Text("data"))
-              ],
+                  // ElevatedButton(
+                  //     onPressed: () async {
+                  //       getValue();
+                  //     },
+                  //     child: Text("data"))
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: Text(
+                      "Daily vibes ✨",
+                      style: GoogleFonts.manrope(
+                          textStyle:
+                              TextStyle(color: Colors.white, fontSize: 30)),
+                    ),
+                  ),
+                  Text(
+                    "A random song for you everyday!",
+                    style: GoogleFonts.manrope(
+                        textStyle:
+                            TextStyle(color: Colors.white, fontSize: 18)),
+                  ),
+                  // Padding(
+                  //   padding: const EdgeInsets.only(
+                  //       top: 10.0, left: 30.0, right: 30.0),
+                  //   child: FutureBuilder(
+                  //     future: getImageURL(),
+                  //     builder: (context, snapshot) {
+                  //       if (snapshot.hasData) {
+                  //         return Image.network(
+                  //           snapshot.data as String,
+                  //           fit: BoxFit.fill,
+                  //           loadingBuilder: (BuildContext context, Widget child,
+                  //               ImageChunkEvent? loadingProgress) {
+                  //             if (loadingProgress == null) return child;
+                  //             return Center(
+                  //               child: CircularProgressIndicator(
+                  //                 value: loadingProgress.expectedTotalBytes !=
+                  //                         null
+                  //                     ? loadingProgress.cumulativeBytesLoaded /
+                  //                         loadingProgress.expectedTotalBytes!
+                  //                     : null,
+                  //               ),
+                  //             );
+                  //           },
+                  //         );
+                  //       } else if (snapshot.hasError) {
+                  //         return Text("Error loading image");
+                  //       } else {
+                  //         return CircularProgressIndicator();
+                  //       }
+                  //     },
+                  //   ),
+                  // ),
+                  // Padding(
+                  //   padding: const EdgeInsets.only(
+                  //       bottom: 10.0, left: 30.0, right: 30.0),
+                  //   child: FutureBuilder(
+                  //     future: getScanCode(),
+                  //     builder: (context, snapshot) {
+                  //       if (snapshot.hasData) {
+                  //         return Image.network(
+                  //           snapshot.data as String,
+                  //           //width: MediaQuery.of(context).size.width * 0.5,
+                  //           fit: BoxFit.fill,
+                  //           loadingBuilder: (BuildContext context, Widget child,
+                  //               ImageChunkEvent? loadingProgress) {
+                  //             if (loadingProgress == null) return child;
+                  //             return Center(
+                  //               child: CircularProgressIndicator(
+                  //                 value: loadingProgress.expectedTotalBytes !=
+                  //                         null
+                  //                     ? loadingProgress.cumulativeBytesLoaded /
+                  //                         loadingProgress.expectedTotalBytes!
+                  //                     : null,
+                  //               ),
+                  //             );
+                  //           },
+                  //         );
+                  //       } else if (snapshot.hasError) {
+                  //         return Text("Error loading image");
+                  //       } else {
+                  //         return CircularProgressIndicator();
+                  //       }
+                  //     },
+                  //   ),
+                  // ),
+                ],
+              ),
             ),
           ),
         ));
